aa7d4b2fc334c4a7bc99ee36c545d0e73b18dc2a
Shubham Kumar <edwssm777@gmail.com>
2021-04-10 00:22:10 +0530

    fetch

---
+++ src/components/navBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="nav">
+    <h1>Nav Bar</h1>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+export default 
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
